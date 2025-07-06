8cc04d2363163aabd925ce641a35606f91f0ffef
HamedKazemii <mr.hamedkazemi@gmail.com>
2025-07-06 19:24:56 +0330

    🐛 fix: correct v2ray command with 'run -config'

--- main.sh
+++ main.sh
@@ -5,5 +5,5 @@ set -e
 openssl req -x509 -newkey rsa:2048 -nodes -keyout /app/key.pem -out /app/cert.pem -days 365 \
   -subj "/C=US/ST=None/L=None/O=None/CN=railway.app"
 
-# اجرای V2Ray با دستور صحیح
+# اجرای صحیح V2Ray
 /app/v2ray/v2ray run -config /app/config.json
